a15971e8e3cca4cc29cddfe0c4da16f56a6e11db
johorin <hoyuak2145@docomo.ne.jp>
2021-02-13 17:19:11 +0900

    フェードイン用のjQueryファイルを作成して講習ページとHOMEページの両方にそれぞれのフェードインの処理を施しました。また、アンカーリンクに対してスクロールのアニメーションを追加しました。

---
+++ jQuery/kosyu_fadeIn.js
@@ -0,0 +1,19 @@
+/*jQuery*/
+$(function(){
+    $(window).on('load scroll', function() {
+       var winScroll = $(window).scrollTop();
+       var winHeight = $(window).height();
+       var scrollPos1 = winScroll + (winHeight * 0.8);  //大きな画像イメージのフェードイン用
+       var scrollPos2 = winScroll + (winHeight * 0.7);  //文字などのフェードイン用
+       $(".show1").each(function() {
+          if($(this).offset().top < scrollPos1) {
+             $(this).css({opacity: 1, transform: 'translate(0, 0)'});
+          }
+       });
+       $(".show2").each(function() {
+        if($(this).offset().top < scrollPos2) {
+           $(this).css({opacity: 1, transform: 'translate(0, 0)'});
+        }
+     });
+    });
+ });
